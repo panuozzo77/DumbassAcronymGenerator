43a4718f4c55044fd8b8ee6e9f7b4dd44967e62e
panuozzo77 <totonnoyou59@gmail.com>
2024-05-31 14:13:29 +0200

    little adjustments

--- src/GrammarChecker.py
+++ src/GrammarChecker.py
@@ -13,31 +13,34 @@ def log(message):
     if verbose:
         print(message)
 
-def analyze_word(word):
-    doc = nlp(word)
-    for token in doc:
-        morph = token.morph.to_dict()
-        type = token.pos_
-        genre = morph.get('Gender', 'N/A')
-        number = morph.get('Number', 'N/A')
+class GrammarChecker:
+    def analyze(self, word):
+        doc = nlp(word)
+        for token in doc:
+            morph = token.morph.to_dict()
+            type = token.pos_
+            genre = morph.get('Gender', 'N/A')
+            number = morph.get('Number', 'N/A')
 
-        log(f"Parola: {token.text}")
-        log(f"Tipo: {type}")
-        if type == 'VERB':
+            log(f"Parola: {token.text}")
+            log(f"Tipo: {type}")
+            if type == 'VERB':
 
-            modo = morph.get('VerbForm', 'N/A')
-            tempo = morph.get('Tense', 'N/A')
-            persona = morph.get('Person', 'N/A')
-            log(f"Modo: {modo}")
-            log(f"Tempo: {tempo}")
-            log(f"Persona: {persona}")
-        log(f"Genere: {genre}")
-        log(f"Numero: {number}")
+                modo = morph.get('VerbForm', 'N/A')
+                tempo = morph.get('Tense', 'N/A')
+                persona = morph.get('Person', 'N/A')
+                log(f"Modo: {modo}")
+                log(f"Tempo: {tempo}")
+                log(f"Persona: {persona}")
+            log(f"Genere: {genre}")
+            log(f"Numero: {number}")
 
-        return type, genre, number
+            return type, genre, number
 
 if __name__ == "__main__":
     #parser = argparse.ArgumentParser(description='Analizzatore morfologico per parole italiane.')
     #parser.add_argument('parola', type=str, help='La parola da analizzare')
     #args = parser.parse_args()
-    analyze_word("rincorrere")
+    gc = GrammarChecker()
+    gc.analyze("mai")
+
